fc4e5a59c226e934fe1e0c5e2f22f72b07bb4d05
FaizBastomi <faizbastomy3@gmail.com>
2021-05-20 00:51:31 +0700

    fix restart

--- commands/Private/restart.js
+++ commands/Private/restart.js
@@ -1,6 +1,6 @@
 /** Only for pm2  users */
 const { owner } = require('../../config.json')
-const { exec } = require('child_process')
+const pm2 = require('pm2')
 
 module.exports = {
     name: 'restart',
@@ -8,9 +8,11 @@ module.exports = {
     description: 'Restart bot.',
     async execute(client, message, args) {
         if (message.author.id !== owner) return message.channel.send('You can\'t execute this command')
-        exec('pm2 restart index', function(err) {
-            if (err) return message.channel.send('Something wrong while restarting bot.')
-            message.channel.send('Sucess restart bot.')
+        pm2.restart('index', (err) => {
+            if (err) {
+                console.log(err)
+            }
         })
+        message.channel.send('sukses')
     }
 }
